a6cce237272ae2880042d44611274b34e82cadee
Carlos Montalvo <cmontalvo@southalabama.edu>
2026-03-03 07:42:55 -0600

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,6 +1,33 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
+  "id": "current-edition",
+  "level": "1",
+  "url": "current-edition.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Current Edition",
+  "body": " Current Edition  This textbook was last updated on . The latest pdf can be found on Github while the most up to date web based version is on Github Pages  "
+},
+{
+  "id": "manuscript-changes",
+  "level": "1",
+  "url": "manuscript-changes.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Manuscript Changes",
+  "body": " Manuscript Changes    Original tutorials in Google Docs created  December 21st, 2021 - Updated links for manuscript and hardware  Tutorials purchased by Tangibles that Teach  May 30th, 2022 - Tangibles that teach went out of business and chapter began the move to Github  June 28th, 2022 - Work began on a Chromebook. Unfortunately the Figures folder is not back up on Git. As such a main_latest.pdf has been created that's the latest full version. The main.pdf is the version created by the Chromebook so it has new chapters but none of the older chapters. Figure files are now backup on Git but only figures from the 'Voltage Potentiometer' are currently there.  July 2nd, 2022 - All figures backed up and latest manuscript completed  October 18th, 2022 - A pedometer lab has been added.  November 8th, 2022 - Edited the Servo and feedback control servo lab to be one big lab with 3 parts  January 8th, 2023 - Removed all mentions of Tangibles that Teach in the main body of the text.  February 6th, 2023 - Changed an assignment description for LEDs and push buttons  February 22nd, 2023 - Moved the Bluetooth module to be before the modules lab so this will end up being a few updates to make those projects more uniform. I also created a backup called Bluetooth original in case you wanted to go back to the other version.  June 16th, 2023 - Cleaned up the ``changes needed\" list and updated Method 3 with some new software updates.  August 9th, 2023 - Edited the preamble for assignments and made a note in the servo lab  September 5th, 2023 - Updated hyperlinks to show up on Microsoft Edge using underlines and different color text.  September 7th, 2023 - Fixed the TL;DR section  September 15th, 2023 - Updated a link in the DAQ lab  September 20th, 2023 - Made the modules installation section a standalone tex file  October 4th, 2023 - Added a new potentiometer photo  November 16th, 2023 - Edited the lists of parts  November 22nd, 2023 - Edited acceleration lab  December 5th, 2023 - Edited the troubleshooting section  January 12th, 2024 - Edited chapters 1 and 2  January 22nd, 2024 - Edited the bootloader update notes in the troubleshooting chatper  January 25th, 2024 - More edits to the bootloader notes  March 20th, 2024 - Added a preamble about where this textbook is located  April 27th, 2024 - Previously edited servo lab and pendulum lab just the assignment portion and then added this change log to the manuscript  May 7th, 2024 - Title page changed and many sections moved around. Added a new results and discussion section following data analysis from course surveys of this course.  August 12th, 2024 - Added a new requirement for the servo lab  September 12th, 2024 - A few edits to assignment descriptions  October 25th, 2024 - Updated the photocell lab to include the number of data points for the histogram  November 11th, 2024 - Updated Method 3 quick list  March 26th, 2025 - Updated servo, pitot probe and photocell assignments  May 24th, 2025 - Made a few changes to chapters 2 and 19 to transition between the projects and the results and discussion about lab at home frameworks and project based teaching. Also overhauled the feedback control project.  June 16th, 2025 - Updated the assignment directions for all projects.  June 17th, 2025 - Added more theory to the photocell, thermistor and pitot probe assignment  June 20th, 2025 - Added more figures and wiring diagrams to many assignments to help students wire up their circuits.  June 21st, 2025 - Added a few problems from a standard instrumentation textbook. Probably need more statistics questions  July 2nd, 2025 - Moved changes needed to the preamble. Added learning objectives and parts list for a few projects that had them missing.  September 26th, 2025 - Updated the potentiometer assignment to include converting voltage to angle in degrees.  October 1st, 2025 - Updated the thermistor modules assignment with the thermistor equations so it's easy to find in two places.  November 12th, 2025 - Fixed an equation, link and figure in the feedback control assignment  November 14th, 2025 - Fixed a broken link  November 20th, 2025 - Added a GPS requirement to the integrating acceleration lab  December 5th, 2025 - Added the settling time to the list of parameters for the time constant assignment and edited the overall assignment directions to have a better description of the video requirements. Also edited the servo second order systems equation for an aircraft to be stable since the equations were unstable open loop. I also moved the changes needed to Github. I also edited the purchase assignment to have bold text for some important information.  February 2nd, 2026 - Updated the push button and LED assignment to fix a mistake in the voltage source description.  February 5th, 2026 - Added an update from Jan 18th that I forgot to push which highlighted the ``Rubric\" for each assignment. Also added a missing grade to the push button and LED assignment.  February 9th, 2026 - Moved the future project ideas to Github  February 26th, 2026 - Edited the trouble shooting guide for updating the CPB bootloader on Windows    "
+},
+{
+  "id": "future-changes",
+  "level": "1",
+  "url": "future-changes.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Changes Needed and Future Project Ideas",
+  "body": " Changes Needed and Future Project Ideas  Needed changes and Future Project Ideas are now tracked on Github  "
+},
+{
   "id": "front-colophon",
   "level": "1",
   "url": "front-colophon.html",
@@ -10,22 +37,31 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-section-name",
+  "id": "introduction",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "introduction.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Introduction",
+  "body": " Introduction  This textbook describes an instrumentation kit used for an Instrumentation and Experimental Methods course at the undergraduate level. This textbook has been designed with the student and faculty member in mind. The kit contains the CircuitPlayground Express\/Bluefruit running CircuitPython and is used to teach fundamentals of instrumentation and provide a hands-on way of learning. Engineering is usually taught in a traditional lecture format, involving theory in the classroom, homework outside of class, and routine examinations. Progressive forms of learning such as flipped classrooms and project based learning (PBL) have created new and fun ways for professors to interact with students and for students to be more involved in their learning. PBL provides a student-centered method of teaching and learning by posing problems for students to solve with the solution of the project being the primary goal and the theory a secondary goal.  "
 },
 {
-  "id": "backmatter-2",
+  "id": "course-description",
   "level": "1",
-  "url": "backmatter-2.html",
-  "type": "Colophon",
-  "number": "",
-  "title": "Colophon",
-  "body": " This book was authored in PreTeXt .  "
+  "url": "course-description.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Course Description",
+  "body": " Course Description  The course is designed to coincide with lecture content in a standard engineering instrumentation course. The students are guided through numerous projects. The specific course learning objectives, which include statistics, dynamic responses, operational amplifiers, and electronic circuits both analog and digital are shown below:  "
+},
+{
+  "id": "acknowledgements",
+  "level": "1",
+  "url": "acknowledgements.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "Acknowledgements",
+  "body": " Acknowledgements   The author, Dr. Carlos Montalvo would like to acknowledge a few key members who made this textbook possible. First and foremost I would like to thank Adafruit for their entire ecosystem of electronics, tutorials, blogs and forums. Much of what I have learned here to teach Instrumentation was from Adafruit and the Adafruit Learn system and specifically people like Lady Ada and John Park who have helped shape CircuitPython and the CircuitPlayground Express to what it is today. I would also like to thank Dr. Saami Yazdani for creating the blueprint for Instrumentation at my university by creating a laboratory environment for an otherwise totally theoretical course. His course was the foundation for this textbook and for that I thank him for showing the way. I’d like to also thank and acknowledge Tangibles that Teach for giving me the opportunity to morph this loose set of projects into a textbook that can be used for multiple universities and classrooms and of course help students learn and acquire knowledge through creating. I'd also like to change Steven Clontz for first showing me PreTeXt and for Oscar Levin and all of his helpful replies via email and Zoom. Finally, I'd like to thank the co-authors Lisa and Marine for helping me add some statistical data to this textbook and being all around great academic colleagues and friends   "
 }
 ]
 
